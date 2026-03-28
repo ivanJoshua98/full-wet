f6c8f8306697bc83f6f4d3fde698fbe019a94144
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2026-03-28 04:37:45 -0300

    Static routes are separated from routes with dynamic content

--- apps/web/src/controllers/seasonsController.ts
+++ apps/web/src/controllers/seasonsController.ts
@@ -10,3 +10,17 @@ export async function getAllSeasons() {
     return [];
   }
 }
+
+export async function getPastSeasons() {
+  try {
+    const response = await fetch(`${API_URL}/seasons`);
+    const data = await response.json();
+    if (data.length > 0) {
+      data.shift();
+    }
+    return data as number[];
+  } catch (error) {
+    console.error("Error al obtener las temporadas:", error);
+    return [];
+  }
+}

9534dff1b6287008a9e1551bf67166c823f61370
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2026-03-31 02:53:46 -0300

    Script command to start server and web dockerfile were updated

--- apps/web/src/controllers/api.ts
+++ apps/web/src/controllers/api.ts
@@ -7,10 +7,10 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 // Carga el .env de la raíz del monorepo, modificar el path de ser necesario
 dotenv.config({ path: path.join(__dirname, "../../../../.env") });
 
-const API_URL = process.env.API_URL;
+const API_URL = import.meta.env.PUBLIC_API_URL;
 
 if (!API_URL) {
-    throw new Error("API_URL is not defined");
+    throw new Error("PUBLIC_API_URL is not defined");
 }
 
 export { API_URL };
